34321739b2d0416baac05655b9eadd6e6ab10dbb
Jivak Sute <jivakysute@gmail.com>
2022-09-04 23:12:42 +0530

    mathmatical oparators problems

---
+++ mathematical-ops/problem2.js
@@ -0,0 +1,13 @@
+var one =1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five =5;
+
+var one = 1*2;
+var two = 2*2;
+var three = 3*2;
+var four = 4*2;
+var five =5*2;
+
+console.log(one+two+three+four+five);
